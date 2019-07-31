970a77dff868a0a3a37bea6cb3155b558dca6abe
nitheeshmashraf <nitheeshmashraf@gmail.com>
2019-07-31 12:46:16 +0400

    Updates

---
+++ precache-manifest.4ad6807c1240cd798fdf615fd36e2c2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d7260ea7e8e984cf1f30265dd0b9ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f55738d4ba639653929b",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "09d2e3e3a2c64d41ca6b",
+    "url": "/portfolio/static/css/main.8b0e16c2.chunk.css"
+  },
+  {
+    "revision": "f55738d4ba639653929b",
+    "url": "/portfolio/static/js/2.61b6dae8.chunk.js"
+  },
+  {
+    "revision": "09d2e3e3a2c64d41ca6b",
+    "url": "/portfolio/static/js/main.ac89ec95.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
